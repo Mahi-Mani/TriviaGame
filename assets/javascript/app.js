5c6228e074debf40ffbc70390653cc53a5cffa6d
Mahi Mani <m.mahishamani@gmail.com>
2019-09-06 15:31:34 -0700

    added three questions coded to calculate score when game is over

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,6 +10,9 @@ var options = [["Jaipur","Kashmir","Chennai","Mumbai"],
 var value;
 var i = 0;
 var j = 0;
+var score = 0;
+var correctAnswers = 0;
+var wrongAnswers = 0;
 
 $(document).ready(function(){
     // Game is started when Start button is pressed
@@ -50,17 +53,25 @@ $(document).ready(function(){
     }
     // Function that displays timeUP when remaining time expires!
     function timeUp(timeRemaining){
+        $("#timeRemaining").show();
+        $("#timeUp").show();
         $("#timeRemaining").html("<p>"+"Time Remaining : 0"+"</p>");
         $("#timeUp").append("<p>Time Up!</p>");
         clearInterval(intervalId);
         askQuestionsHide();
     }
 
+    function allHide(){
+        $("#timeRemaining").hide();
+        $("#timeUp").hide();
+        $("#answer").hide();
+    }
+
     // Function that displays questions to user
     function askQuestions(){
         // $("#questions").html("<p>What is Pink City</p>");
         // i=0;
-        if(i <= quesBank.length){
+        
         $("#answer").hide();
         $("#quesBtn").show();
         $("#ans1").show();
@@ -80,42 +91,44 @@ $(document).ready(function(){
       $("#ans4").html("<button type='button' class='btn btn-secondary'id='ans4'value='" + options[i][3] + "'>"+options[i][3]+"</button>");
 
       $("#ans1").on("click","button[id=ans1]",function(){
+        $("#ans1").unbind('click'); //Resets button
           var answer1 = $(this).val();
         //   console.log($("#ans1").text(value));
         console.log(answer1);
-        $("#ans1").unbind('click'); //Resets button
         evaluate(answer1);
 
       });
 
       $("#ans2").on("click","button[id=ans2]",function(){
+        $("#ans2").unbind('click'); //Resets button
         var answer2 = $(this).val();
       //   console.log($("#ans1").text(value));
-      $("#ans2").unbind('click'); //Resets button
       console.log(answer2);
       evaluate(answer2);
 
     });
 
     $("#ans3").on("click","button[id=ans3]",function(){
+        $("#ans3").unbind('click'); //Resets button
         var answer3 = $(this).val();
       //   console.log($("#ans1").text(value));
       console.log(answer3);
-      $("#ans3").unbind('click'); //Resets button
+    //   $("#ans3").unbind('click'); //Resets button
       evaluate(answer3);
 
     });
 
     $("#ans4").on("click","button[id=ans4]",function(){
+        $("#ans4").unbind('click'); //Resets button
         var answer4 = $(this).val();
       //   console.log($("#ans1").text(value));
       console.log(answer4);
-      $("#ans4").unbind('click'); //Resets button
+    //   $("#ans4").unbind('click'); //Resets button
       evaluate(answer4);
 
     });
         
-    }
+    
 }
     // Function that evaluates user's answer
     function evaluate(answer){
@@ -128,9 +141,19 @@ $(document).ready(function(){
             console.log("inside equal if loop: "+answer);
             askQuestionsHide();
             stopTimer();
-            if(i!=1) i++;
+            // scoreCard();
+            score = score + 1;
+            correctAnswers++;
+            i++;
+            //Breaks the loop if all questions in array were asked
+            if((i+1) <= quesBank.length){
             setTimeout(askQuestions,5000);
             setTimeout(run,5000);
+        }
+            else{
+            console.log("Game over");
+            gameOver();
+            }
         }
         else if(answer != ansBank[i]){
             $("#answer").html("<p>Oops! Wrong answer</p>");
@@ -139,9 +162,19 @@ $(document).ready(function(){
             console.log("Prints i inside second if "+i);
             askQuestionsHide();
             stopTimer();
+            // calcWrongAnswers();
+            wrongAnswers = wrongAnswers + 1;
             i++;
+            //Breaks the loop if all questions in array were asked
+            if((i+1) <= quesBank.length)
+            {
             setTimeout(askQuestions,5000);
             setTimeout(run,5000);
+            }
+            else{
+                console.log("Game over");
+                gameOver();
+            }
 
         }
         else{
@@ -149,7 +182,18 @@ $(document).ready(function(){
         }
 
     }
+    // Function that calculates score
+    function scoreCard(){
+        $("#score").html("Your total score is : "+score);
+        $("#corrAns").html("Number of correct answers : "+correctAnswers);
 
+ }
+ //Function that keeps track of wrong answers
+ function calcWrongAnswers(){
+     
+     $("#wrongAns").html("Number of wrong answers : "+wrongAnswers);
+
+ }
     // Code that hides questions when time is up and answer is evaluated!
     function askQuestionsHide(){
         $("#quesBtn").hide();
@@ -161,6 +205,13 @@ $(document).ready(function(){
     // Function to stop timer
     function stopTimer(){
     clearInterval(intervalId);
+    }
+    // Function to show score card stuffs to user
+    function gameOver(){
+        $("#gameOver").html("Game Over !");
+        scoreCard();
+        calcWrongAnswers();
+        allHide();
     }
 
     
