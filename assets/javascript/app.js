76b36b967e79ddf446c96258742db3a4d454d851
Mahi Mani <m.mahishamani@gmail.com>
2019-09-08 22:11:58 -0700

    Added styles to quiz game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,11 +2,22 @@
 var intervalId;
 var timeRemaining;
 var questions;
-var quesBank = ["Which city is known as Pink City","Which country is called as The Land of rising Sun","The Land of midnight Sun"];
-var ansBank = ["Jaipur","Japan","Norway"];
+var index
+var questionOrder = [0,1,2,3,4,5,6,7,8,9];
+var quesBank = ["Which city is known as Pink City","Which country is called as The Land of rising Sun","The Land of midnight Sun"
+                ,"City of Roses","Which of the following is not a sister city of Portland","The city of Light","Capital city of Peru"
+                ,"Largest island in Mediterranean Sea","Most populous US State","Largest of Canary Islands"];
+var ansBank = ["Jaipur","Japan","Norway","Portland","Seoul","Paris","Lima","Sicily","California","Tenerife"];
 var options = [["Jaipur","Kashmir","Chennai","Mumbai"],
                 ["Norway","Japan","India","Australia"],
-                ["Indonesia","Canada","Norway","Russia"]];
+                ["Indonesia","Canada","Norway","Russia"],
+                ["Hillsboro","Portland","New York","San Jose"],
+                ["Ashkelon","Bologna","Seoul","Suzhou"],
+                ["New York","Paris","Sydney","Boston"],
+                ["Lima","Callao District","Iquitos","Piura"],
+                ["Mallorca","Ibiza","Capri","Sicily"],
+                ["Oregon","New York State","California","Texas"],
+                ["Tenerife","Las Palmas","Lanzarote","El Hierro"]];
 var value;
 var i;
 var j;
@@ -14,6 +25,7 @@ var score;
 var correctAnswers;
 var wrongAnswers;
 var unanswered;
+var lastNum = 0;
 
 $(document).ready(function(){
     // Game is started when Start button is pressed
@@ -59,6 +71,7 @@ $(document).ready(function(){
     }
 
     function startGame(){
+        randomizeOrder();
         initializeVariables();
         hideVariables();
         $("#startBtn").hide();
@@ -120,17 +133,18 @@ $(document).ready(function(){
         $("#ans2").show();
         $("#ans3").show();
         $("#ans4").show();
+        index = randomQuestion();
         $("#quesBtn").removeClass('d-none');
         $("#quesBtn").html("<button type= 'button' class='list-group-item list-group-item-action active' id='quesBtn'>"+
-        quesBank[i]+"</button>");
+        quesBank[index]+"</button>");
       $("#ans1").removeClass('d-none');
-      $("#ans1").html("<button type='button' class='btn btn-secondary'id='ans1'value='" + options[i][0] + "'>"+options[i][0]+"</button>");
+      $("#ans1").html("<button type='button' class='btn btn-secondary'id='ans1'value='" + options[index][0] + "'>"+options[index][0]+"</button>");
       $("#ans2").removeClass('d-none');
-      $("#ans2").html("<button type='button' class='btn btn-secondary'id='ans2'value='" + options[i][1] + "'>"+options[i][1]+"</button>");
+      $("#ans2").html("<button type='button' class='btn btn-secondary'id='ans2'value='" + options[index][1] + "'>"+options[index][1]+"</button>");
       $("#ans3").removeClass('d-none');
-      $("#ans3").html("<button type='button' class='btn btn-secondary'id='ans3'value='" + options[i][2] + "'>"+options[i][2]+"</button>");
+      $("#ans3").html("<button type='button' class='btn btn-secondary'id='ans3'value='" + options[index][2] + "'>"+options[index][2]+"</button>");
       $("#ans4").removeClass('d-none');
-      $("#ans4").html("<button type='button' class='btn btn-secondary'id='ans4'value='" + options[i][3] + "'>"+options[i][3]+"</button>");
+      $("#ans4").html("<button type='button' class='btn btn-secondary'id='ans4'value='" + options[index][3] + "'>"+options[index][3]+"</button>");
 
       $("#ans1").unbind("click").on("click", "button[id=ans1]",function (){
         // $("#ans1").unbind('click'); //Resets button
@@ -175,13 +189,33 @@ $(document).ready(function(){
         
     
 }
+
+    //Function to randomize the question order
+    function randomizeOrder(){
+    var tempVar;
+    for(i=0;i<=(quesBank.length*3);i++)
+    {
+    var min = 0;
+    var max = quesBank.length;
+    random1 = Math.floor(Math.random() * (max - min)) + min;
+    random2 = Math.floor(Math.random() * (max - min)) + min;
+    tempVar = questionOrder[random1];
+    questionOrder[random1]=questionOrder[random2];
+    questionOrder[random2]=tempVar;
+    console.log("Random Order Array: "+questionOrder);
+    }
+    }
+    // Function to ask random question (ie) not in order
+    function randomQuestion(){
+        return questionOrder[i];
+}
     // Function that evaluates user's answer
     function evaluate(answer){
         $("#answer").removeClass('d-none');
         $("#answer").show();
-        if(answer == ansBank[i]){
+        if(answer == ansBank[index]){
             console.log("Answer inside equal if loop : "+answer);
-            console.log("Inside equal if loop: "+ansBank[i]+"value of i is : "+i);
+            console.log("Inside equal if loop: "+ansBank[index]+"value of i is : "+i);
             // $(answer).addClass("active");
             $("#answer").html("<p>Yay! Correct answer</p>");
             console.log("inside equal if loop: "+answer);
@@ -194,7 +228,7 @@ $(document).ready(function(){
             //Breaks the loop if all questions in array were asked
             ifAllQuestionsAsked();
         }
-        else if(answer != ansBank[i]){
+        else if(answer != ansBank[index]){
             $("#answer").html("<p>Oops! Wrong answer</p>");
             console.log("Prints i inside second if "+i);
             // $("#answer").append("<p>Correct answer is " +ansBank[i]+"</p>");
@@ -216,23 +250,23 @@ $(document).ready(function(){
     function ifAllQuestionsAsked(){ 
         if((i+1) <= quesBank.length)
         {
-        setTimeout(askQuestions,5000);
-        setTimeout(run,5000);
+        setTimeout(askQuestions,2000);
+        setTimeout(run,2000);
         // evaluateLastQuestion();
         }
 
         else{
             console.log("Game over");
             // gameOver();
-            setTimeout(gameOver,5000);
+            setTimeout(gameOver,2000);
         }
     }
     // Function that shows correct answer when answered incorrectly
     function showCorrectAnswer(){
         $("#answer").removeClass('d-none');
         $("#answer").show();
-        console.log("Show correct answer"+ansBank[i]);
-        $("#answer").append("<p>Correct answer is " +ansBank[i]+"</p>");
+        console.log("Show correct answer"+ansBank[index]);
+        $("#answer").append("<p>Correct answer is " +ansBank[index]+"</p>");
         
     }
     // Function that calculates score
@@ -251,7 +285,7 @@ $(document).ready(function(){
  
  function calcUnanswered(){
      showVariables();
-     $("#unAns").html("Number of questions unanswered : "+unanswered);
+     $("#unAns").html("Number of unanswered : "+unanswered);
  }
     // Code that hides questions when time is up and answer is evaluated!
     function askQuestionsHide(){
@@ -268,7 +302,12 @@ $(document).ready(function(){
     // Function to show score card stuffs to user
     function gameOver(){
         $("#gameOverBoot").removeClass('d-none');
+        if(score == quesBank.length){
+            $("#gameOver").html("Congrats !");
+        }
+        else{
         $("#gameOver").html("Game Over !");
+        }
         scoreCard();
         calcWrongAnswers();
         calcUnanswered();
@@ -279,7 +318,7 @@ $(document).ready(function(){
     function restartGame(){
         $("#restart").removeClass('d-none');
         $("#restartBtn").removeClass('d-none');
-        $("#restart").html("Restart");
+        $("#restart").html("RESTART");
         $("#restart").on("click",function(){
         $("#timeRemaining").removeClass('d-none');
         $("#timeRemaining").show();
